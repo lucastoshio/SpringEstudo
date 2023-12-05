117362a6c9f94558e0dda61693c0d7d8a9d88e99
Lucas Toshio <lucasmiki@outlook.com>
2023-12-05 01:18:26 -0300

    Add files via upload

---
+++ src/teste/Teste.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		System.out.println("olá mundo ");
+		nothing to commit, working tree clean
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
